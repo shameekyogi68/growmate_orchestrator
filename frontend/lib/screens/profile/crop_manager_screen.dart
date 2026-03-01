163dd9233726382d9a4760f8a8fee70da53310ec
Shameek Yogi <shameekyogiofficial@gmail.com>
2026-03-01 22:17:51 +0530

    fix(frontend): Add missing SharedPreferences import in CropManagerScreen

--- frontend/lib/screens/profile/crop_manager_screen.dart
+++ frontend/lib/screens/profile/crop_manager_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import '../../core/theme/growmate_theme.dart';
 import '../../core/services/api_service.dart';
 import '../../core/models/api_models.dart';
