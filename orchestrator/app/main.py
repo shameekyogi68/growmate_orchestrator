b46f22f2dab2535a23bfe5c2bc353ba0422c5e1e
Shameek Yogi <shameekyogiofficial@gmail.com>
2026-03-01 20:35:21 +0530

    fix(orchestrator): Enable universal CORS for Flutter Web

--- orchestrator/app/main.py
+++ orchestrator/app/main.py
@@ -53,12 +53,7 @@ app.add_middleware(GZipMiddleware, minimum_size=500)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://localhost:8080",
-        "https://growmate-web.vercel.app",
-        "https://growmate.shameekyogi.com"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
